f0e1b4878acc17e4ef518e07b93b0ca64d249e14
Nour <>
2021-01-12 13:08:02 -0500

    nth

---
+++ Radix.java
@@ -0,0 +1,18 @@
+public class Radix{
+  public static int nth(int n, int col){
+    int digit;
+    digit = n / (10^col);
+    digit = digit%10;
+
+    return digit;
+  }
+
+  public static int length(int n){
+    return -1;
+  }
+
+  public static void merge(MyLinkedList original, MyLinkedList[] buckets){
+
+  }
+
+}
