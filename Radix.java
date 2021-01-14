1b986bb9822e38469759218459b376bcb711d3ef
Nour <>
2021-01-13 21:33:54 -0500

    fixed issue with negatives for nth

--- Radix.java
+++ Radix.java
@@ -1,7 +1,7 @@
 public class Radix{
   public static int nth(int n, int col){
     int digit;
-    digit = n / (int)(Math.pow(10, col));
+    digit = Math.abs(n / (int)(Math.pow(10, col)));
     digit = digit%10;
 
     return digit;
@@ -19,6 +19,25 @@ public class Radix{
 
   public static void merge(MyLinkedList original, MyLinkedList[] buckets){
 
+  }
+
+  public static void main(String[] args){
+    System.out.println("TESTING NTH");
+    System.out.println();
+    System.out.println(nth(123, 1)==2);
+    System.out.println(nth(-123, 1)==2);
+    System.out.println(nth(123, 2)==1);
+    System.out.println(nth(-123,2)==1);
+
+    System.out.println();
+    System.out.println();
+
+    System.out.println("TESTING LENGTH");
+    System.out.println();
+    System.out.println(length(0)==1);
+    System.out.println(length(15)==2);
+    System.out.println(length(-10)==2);
+    System.out.println(length(5112)==4);
   }
 
 }
