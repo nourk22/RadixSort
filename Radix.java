28554d295591859c8c90bb14b8d9cdf1e6ffe904
Nour <>
2021-01-13 23:37:50 -0500

    both sorts work

--- Radix.java
+++ Radix.java
@@ -33,8 +33,8 @@ public class Radix{
 
     for (int i = 0; i<passes; i++){
 
-      for (int j=0; j<data.size(); j++){
-        int curr = data.remove(j);
+      while (data.size() > 0){
+        int curr = data.remove(0);
 
         if (i==0){
           if (passes < length(curr)){
@@ -42,8 +42,13 @@ public class Radix{
           }
         }
 
-        int digit = nth(j, curr);
-        buckets[digit].add(curr);
+        int digit = nth(curr, i);
+
+        if (curr<0){
+          buckets[9-digit].add(curr);
+        }else{
+          buckets[digit].add(curr);
+        }
 
       }
 
@@ -52,23 +57,70 @@ public class Radix{
     }
   }
 
+  public static void radixSort(SortableLinkedList data){
+    SortableLinkedList neg = new SortableLinkedList();
+    SortableLinkedList pos = new SortableLinkedList();
+
+    while (data.size()>0){
+      int curr = data.remove(0);
+      if (curr>=0){
+        pos.add(curr);
+      }else{
+        neg.add(curr);
+      }
+    }
+
+    radixSortSimple(pos);
+    radixSortSimple(neg);
+
+
+    neg.extend(pos);
+    data.extend(neg);
+
+  }
+
   public static void main(String[] args){
-    System.out.println("TESTING NTH");
-    System.out.println();
-    System.out.println(nth(123, 1)==2);
-    System.out.println(nth(-123, 1)==2);
-    System.out.println(nth(123, 2)==1);
-    System.out.println(nth(-123,2)==1);
+  //  System.out.println("TESTING NTH");
+  //  System.out.println();
+  //  System.out.println(nth(123, 1)==2);
+  //  System.out.println(nth(-123, 1)==2);
+  //  System.out.println(nth(123, 2)==1);
+  //  System.out.println(nth(-123,2)==1);
+  //  System.out.println(nth(198, 3));
 
-    System.out.println();
-    System.out.println();
+  //  System.out.println();
+  //  System.out.println();
 
-    System.out.println("TESTING LENGTH");
+  /*  System.out.println("TESTING LENGTH");
     System.out.println();
     System.out.println(length(0)==1);
     System.out.println(length(15)==2);
     System.out.println(length(-10)==2);
     System.out.println(length(5112)==4);
+    */
+
+  /*  SortableLinkedList test = new SortableLinkedList();
+    test.add(10);
+    test.add(-10);
+    test.add(2);
+    test.add(-3);
+    test.add(5);
+    test.add(-4);
+    test.add(3);
+    test.add(-5);
+    test.add(4);
+    test.add(-2);
+    test.add(0);
+    test.add(101);
+    test.add(-101);
+    test.add(11);
+    test.add(-14);
+    test.add(23);
+    test.add(-34932);
+
+    radixSort(test);
+    System.out.println(test);
+  */
   }
 
 }
